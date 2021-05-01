1e4e44f7431b6041cd1b27626ebd855b4eed26a3
Michael <michael.richards3@my.jcu.edu.au>
2021-05-01 16:19:02 +1000

    heyyyyy, import isn't cursed anymore. BUT THE CURSEDUTILS WILL ALWAYS BE CUUUUUURRRRRSSSSEEDDD!

--- cursedutils/cursed.py
+++ cursedutils/cursed.py
@@ -26,8 +26,10 @@ def Switch():
       elif not case.state:case.state =- 1;f(*u,**ck)
     return switch, case, otherwise
 class struct:
+ gulag=type("sentinal",(),{})()
+ def __init__(self,name="<struct>"):self.__name__=name
  @classmethod
- def bootstrap(c,__g):
+ def bootstrap1(c,__g):
   try:assert __g
   except AssertionError as e:__g=e.__traceback__.tb_frame.f_back.f_globals
   @lambda f:setattr(__import__('ctypes').py_object.from_address(id(__g)+8),'value',type("dict",(dict,),{'__missing__':f}))
@@ -36,7 +38,30 @@ class struct:
    except:self[key]=c()
    return self[key]
  def __getattr__(self,v):
-  try:return(f:=lambda:object.__getattr__(self,v))()
+  try:return(f:=lambda:object.__getattribute__(self,v))()
   except AttributeError:setattr(self,v,self.__class__())
   return f()
+ def __setattr__(self,n,v):
+  if isinstance(v,(lambda:None).__class__):
+    victim = __import__('ctypes').py_object.from_address(id(self)+8)
+    victim.value = type("struct",(victim.value,),{n:v})
+  elif isinstance(v,print.__class__):
+    victim = __import__('ctypes').py_object.from_address(id(self)+8)
+    victim.value = type("struct",(victim.value,),{n:lambda s,*a,**k:v(*a,**k)})#bound builtin behaviour
+  else:object.__setattr__(self,n,v)
+ __repr__=lambda s:f"<struct object {s.__name__} 1x{len(dir(self))}"
+ @classmethod
+ def bootstrap2(self,gl):
+  try:assert gl
+  except Exception as e:gl=e.__traceback__.tb_frame.f_back.f_globals
+  hack=lambda o:__import__('ctypes').c_void_p.from_address(id(o)+8)
+  prisoner=hack(gl).value
+  self.gulag=type('atexit',(),{'__del__':lambda s:setattr(hack(gl),'value',prisoner)})()
+  rename=lambda name:lambda o:setattr(o,'__name__',name)or o
+  @lambda f:setattr(hack(gl),'value',type("matlabinated_globals",(prisoner,),{f.__name__:f}))
+  def __missing__(self, key):
+    try:return getattr(self['__builtins__'],key)
+    except AttributeError:return(lambda s:self.__setitem__(key,s))(rename(key)(struct()))
+  gl.update({'struct':struct})
+  return gl
 

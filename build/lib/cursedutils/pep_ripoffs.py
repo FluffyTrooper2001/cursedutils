46114f5ef71ffdc5b00e64f2de103efd5976f2c0
Michael Richards <fluffy.trooper.2001@gmail.com>
2021-08-13 12:03:57 +1000

    -=-=-=-=-=-VERY EXCELENT COMIT MESEG-=-=-=-=-=-=-
    make this look neater, i'm thinking of adding more, when i get around to it.

--- build/lib/cursedutils/pep_ripoffs.py
+++ build/lib/cursedutils/pep_ripoffs.py
@@ -1,12 +1,12 @@
-from.wrappers import*
 #PEP 3150 "given" status=deferred
-class PEP3150_Given_BaseClass:
- def __init__(s,**k):[setattr(s,this,that)for this,that in k.items()]
 class PEP3150_Given_MetaClass(type):
  def __init__(cls,name,parents,attrs):
-  for i,attr in enumerate(attrs):
-    if hasattr(attr,'__call__'):attrs[i]=attr()
+  for name,attr in attrs.items():
+    try:attrs[name] = property(attr)
+    except Exception:pass
   super().__init__(name,parents,attrs)
+class Given(metaclass = PEP3150_Given_MetaClas):
+ def __init__(s,**k):[setattr(s,this,that)for this,that in k.items()]
 def _pep3150_example(a=4,b=5,op=lambda x,y:x*y):
  """PEP 3150 example:
 op(?.f, ?.g) given bound_a=a, bound_b=b in:
@@ -15,8 +15,9 @@ op(?.f, ?.g) given bound_a=a, bound_b=b in:
     def g():
         return bound_a - bound_b
 """
- @execute_with(bound_a=a,bound_b=b)
- class given(PEP3150_Given_BaseClass,metaclass=PEP3150_Given_MetaClass):
+ call = lambda*a,**k:lambda f:f (*a,**k)
+ @call(bound_a=a,bound_b=b)
+ class given(Given):
   def f(s):return s.bound_a + s.bound_b
   def g(s):return s.bound_a - s.bound_b
  return op(given.f,given.g)

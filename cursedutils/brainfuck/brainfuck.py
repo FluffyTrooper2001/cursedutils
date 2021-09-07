c840813bd52b32bed41a9fcd0829383f371397bc
Michael Richards <fluffy.trooper.2001@gmail.com>
2021-09-07 14:37:04 +1000

    remove unnecessary whitespace

--- cursedutils/brainfuck/brainfuck.py
+++ cursedutils/brainfuck/brainfuck.py
@@ -68,46 +68,20 @@ print()
 chars = '+-<>.,[]'
 
 def call(self,code,input=input):
-    from collections import defaultdict as d
-    l=input==__builtins__.input
+    from collections import defaultdict as d;l=input==__builtins__.input
     if not l:input=lambda i=1:input.pop(i-1)
-    a,o=d(int),''
-    i=p=t=0
+    a,o=d(int),'';i=p=t=0
     for c in iter(lambda:code[p],''):
-        def __0(i,_):
-            a[i]+=1
-            a[i]%=256
-            return i,_,0
-        def __1(i,_):
-            a[i]-=1
-            a[i]%=256
-            return i,_,0
-        def __2(i,o):
-            o+=chr(a[i])
-            return i,o,0
-        def __3(i,_):
-            a[i]+=ord(input(1))%256
-            return i,_,0
-        f = [__0,__1,
-             lambda i,_:(i+1,_,0),
-             lambda i,_:(i-1,_,0),
-             __2,__3,
-             lambda i,o:(i,o,int(not a[i])),
-             lambda i,o:(i,o,---bool(a[i])),
-             lambda*_:(*_,0)][chars.find(c)]
-        if t:
-            t+=(c=='[')-(c==']')
-        else:
-            i,o,t=f(i,o)
-        p+=1-2*(t<0)
-        try:code[p]
+        def __0(i,_):a[i]=(1+a[i])%256;return i,_,0
+        def __1(i,_):a[i]-=1;a[i]%=256;return i,_,0
+        def __2(i,o):o+=chr(a[i]);return i,o,0
+        def __3(i,_):a[i]+=ord(input(1))%256;return i,_,0
+        f=[__0,__1,lambda i,_:(i+1,_,0),lambda i,_:(i-1,_,0),__2,__3,lambda i,o:(i,o,int(not a[i])),lambda i,o:(i,o,---bool(a[i])),lambda*_:(*_,0)][chars.find(c)]
+        if t:t+=(c=='[')-(c==']')
+        else:i,o,t=f(i,o)
+        try:p+=1-2*(t<0);code[p]
         except IndexError:break
     return o
-
 @lambda c:c()
-class brainfuck:
-  def __format__(self, spec):
-    return self(code=spec)
-  __call__ = call
-
+class brainfuck:__call__,__format__=call,lambda s,p:s(c)
 print(f"{brainfuck:++++++++++[>+++>+++++++>++++++++>++++++++++>+++++++++++<<<<<-]>>++.-->>+.+++++++..-------->+This is a comment.-<<<<++.-->>+++++++.------->>+.+++.----<++++++++.--------.<<<+++.}")
